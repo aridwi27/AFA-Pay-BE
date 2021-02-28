ec99a8344733b5f4bc018959229f89918c7e5545
alifma <alifmaulana26@gmail.com>
2021-02-28 19:26:57 +0700

    Release Branch Fix

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser')
 const http = require('http')
 const userRoute = require('./src/routes/users')
 const transactionRoute = require('./src/routes/transactions')
-
+// History Router
+const history = require('connect-history-api-fallback')
 const app = express()
 
 
@@ -17,6 +18,15 @@ app.use(userRoute)
 app.use(transactionRoute)
 // open route for public image
 app.use('/images', express.static('./public/images'))
+
+// API History
+app.use(history({
+    verbose: true
+}))
+
+
+// Deploy FrontEndPath
+app.use('/', express.static('./dist'))
 
 require('dotenv').config()
 
